21d38a8135352c6a24f3d0a7a9c2c50b28a630a9
Cayter Goh <cayter1986@gmail.com>
2019-10-01 22:58:32 +0800

    Add more tests

--- middleware/request_logger_test.go
+++ middleware/request_logger_test.go
@@ -1,1 +1,39 @@
 package middleware
+
+import (
+	"crypto/tls"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/appist/appy/support"
+	"github.com/appist/appy/test"
+	"github.com/gin-gonic/gin"
+)
+
+func TestFilterParams(t *testing.T) {
+	assert := test.NewAssert(t)
+	recorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(recorder)
+	ctx.Request = &http.Request{
+		Host:       "0.0.0.0",
+		Method:     "GET",
+		Proto:      "HTTP/2.0",
+		RemoteAddr: "0.0.0.0",
+		TLS:        &tls.ConnectionState{},
+		URL:        &url.URL{},
+	}
+	RequestLogger(support.Config)(ctx)
+
+	host := "http://0.0.0.0"
+	query := "username=user&password=secret"
+	request := &http.Request{
+		RequestURI: host + "?" + query,
+	}
+	request.URL = &url.URL{
+		RawQuery: query,
+	}
+
+	assert.Contains(filterParams(request, support.Config), "password=[FILTERED]")
+}
