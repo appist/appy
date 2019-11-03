afa5c595561aa71d5c9d0234db0234c9c93626a6
Cayter Goh <cayter1986@gmail.com>
2019-11-03 23:52:56 +0800

    Add more tests

--- config_test.go
+++ config_test.go
@@ -2,7 +2,9 @@ package appy
 
 import (
 	"os"
+	"reflect"
 	"testing"
+	"time"
 )
 
 type ConfigSuite struct {
@@ -18,23 +20,137 @@ func (s *ConfigSuite) SetupTest() {
 		"locale": "testdata/pkg/locales",
 		"view":   "testdata/pkg/views",
 	}
-	os.Setenv("APPY_MASTER_KEY", "481e5d98a31585148b8b1dfb6a3c0465")
 }
 
 func (s *ConfigSuite) TearDownTest() {
 	_ssrPaths = s.oldSSRPaths
+}
+
+func (s *ConfigSuite) TestNewConfigDefaultValue() {
+	os.Setenv("APPY_MASTER_KEY", "481e5d98a31585148b8b1dfb6a3c0465")
+
+	tt := map[string]interface{}{
+		"AppyEnv":                         "development",
+		"HTTPDebugEnabled":                false,
+		"HTTPLogFilterParameters":         []string{"password"},
+		"HTTPHealthCheckURL":              "/health_check",
+		"HTTPHost":                        "localhost",
+		"HTTPPort":                        "3000",
+		"HTTPGracefulTimeout":             30 * time.Second,
+		"HTTPIdleTimeout":                 75 * time.Second,
+		"HTTPMaxHeaderBytes":              0,
+		"HTTPReadTimeout":                 60 * time.Second,
+		"HTTPReadHeaderTimeout":           60 * time.Second,
+		"HTTPWriteTimeout":                60 * time.Second,
+		"HTTPSSLEnabled":                  false,
+		"HTTPSSLCertPath":                 "./tmp/ssl",
+		"HTTPSessionCookieDomain":         "localhost",
+		"HTTPSessionCookieHTTPOnly":       true,
+		"HTTPSessionCookieMaxAge":         0,
+		"HTTPSessionCookiePath":           "/",
+		"HTTPSessionCookieSecure":         false,
+		"HTTPSessionRedisAddr":            "localhost:6379",
+		"HTTPSessionRedisAuth":            "",
+		"HTTPSessionRedisDb":              "0",
+		"HTTPSessionRedisMaxActive":       0,
+		"HTTPSessionRedisMaxIdle":         32,
+		"HTTPSessionRedisIdleTimeout":     30 * time.Second,
+		"HTTPSessionRedisMaxConnLifetime": 0 * time.Second,
+		"HTTPSessionRedisWait":            true,
+		"HTTPSessionName":                 "_session",
+		"HTTPSessionProvider":             "cookie",
+		"HTTPSessionSecrets":              [][]byte{},
+		"HTTPAllowedHosts":                []string{},
+		"HTTPCSRFCookieDomain":            "localhost",
+		"HTTPCSRFCookieHTTPOnly":          true,
+		"HTTPCSRFCookieMaxAge":            43200,
+		"HTTPCSRFCookieName":              "_csrf_token",
+		"HTTPCSRFCookiePath":              "/",
+		"HTTPCSRFCookieSecure":            false,
+		"HTTPCSRFFieldName":               "authenticity_token",
+		"HTTPCSRFRequestHeader":           "X-CSRF-Token",
+		"HTTPCSRFSecret":                  []byte{},
+		"HTTPSSLRedirect":                 false,
+		"HTTPSSLTemporaryRedirect":        false,
+		"HTTPSSLHost":                     "",
+		"HTTPSTSSeconds":                  int64(0),
+		"HTTPSTSIncludeSubdomains":        false,
+		"HTTPFrameDeny":                   false,
+		"HTTPCustomFrameOptionsValue":     "",
+		"HTTPContentTypeNosniff":          false,
+		"HTTPBrowserXSSFilter":            false,
+		"HTTPContentSecurityPolicy":       "",
+		"HTTPReferrerPolicy":              "",
+		"HTTPIENoOpen":                    false,
+		"HTTPSSLProxyHeaders":             map[string]string{},
+	}
+
+	build := DebugBuild
+	logger := NewLogger(build)
+	config := NewConfig(build, logger, nil)
+	cv := reflect.ValueOf(*config)
+	for key, defaultVal := range tt {
+		fv := cv.FieldByName(key)
+
+		// An exception case to handle a different host in test for Github actions.
+		if key == "HTTPSessionRedisAddr" && os.Getenv("HTTP_SESSION_REDIS_ADDR") != "" {
+			s.Equal(fv.Interface(), os.Getenv("HTTP_SESSION_REDIS_ADDR"))
+			continue
+		}
+
+		switch fv.Kind() {
+		case reflect.Map:
+			switch fv.Type().String() {
+			case "map[string]string":
+				for key, val := range fv.Interface().(map[string]string) {
+					s.Equal(val, defaultVal.(map[string]string)[key])
+				}
+			}
+		case reflect.Slice, reflect.Array:
+			switch fv.Type().String() {
+			case "[]string":
+				s.Equal(len(fv.Interface().([]string)), len(defaultVal.([]string)))
+
+				for idx, val := range fv.Interface().([]string) {
+					s.Equal(val, defaultVal.([]string)[idx])
+				}
+			case "[]uint8":
+				s.Equal(len(fv.Interface().([]uint8)), len(defaultVal.([]uint8)))
+
+				for idx, val := range fv.Interface().([]uint8) {
+					s.Equal(val, defaultVal.([]uint8)[idx])
+				}
+			case "[][]uint8":
+				s.Equal(len(fv.Interface().([][]uint8)), len(defaultVal.([][]uint8)))
+
+				for idx, val := range fv.Interface().([][]uint8) {
+					s.Equal(val, defaultVal.([][]uint8)[idx])
+				}
+			default:
+				s.Equal(fv.Interface(), defaultVal)
+			}
+		default:
+			s.Equal(fv.Interface(), defaultVal)
+		}
+	}
+
 	os.Unsetenv("APPY_MASTER_KEY")
 }
 
 func (s *ConfigSuite) TestNewConfigWithoutSettingRequiredConfig() {
+	os.Setenv("APPY_MASTER_KEY", "481e5d98a31585148b8b1dfb6a3c0465")
+
 	build := DebugBuild
 	logger := NewLogger(build)
 	config := NewConfig(build, logger, nil)
 	s.NotNil(config.Errors())
 	s.EqualError(config.Errors()[0], `required environment variable "HTTP_SESSION_SECRETS" is not set. required environment variable "HTTP_CSRF_SECRET" is not set`)
+
+	os.Unsetenv("APPY_MASTER_KEY")
 }
 
 func (s *ConfigSuite) TestNewConfigWithSettingRequiredConfig() {
+	os.Setenv("APPY_MASTER_KEY", "481e5d98a31585148b8b1dfb6a3c0465")
 	os.Setenv("HTTP_CSRF_SECRET", "481e5d98a31585148b8b1dfb6a3c0465")
 	os.Setenv("HTTP_SESSION_SECRETS", "481e5d98a31585148b8b1dfb6a3c0465")
 
@@ -43,8 +159,71 @@ func (s *ConfigSuite) TestNewConfigWithSettingRequiredConfig() {
 	config := NewConfig(build, logger, nil)
 	s.Nil(config.Errors())
 
+	os.Unsetenv("APPY_MASTER_KEY")
 	os.Unsetenv("HTTP_CSRF_SECRET")
 	os.Unsetenv("HTTP_SESSION_SECRETS")
+}
+
+func (s *ConfigSuite) TestNewConfigWithUnparsableEnvVariable() {
+	os.Setenv("APPY_MASTER_KEY", "481e5d98a31585148b8b1dfb6a3c0465")
+	os.Setenv("HTTP_DEBUG_ENABLED", "nil")
+
+	build := DebugBuild
+	logger := NewLogger(build)
+	config := NewConfig(build, logger, nil)
+	s.Contains(config.Errors()[0].Error(), `strconv.ParseBool: parsing "nil": invalid syntax.`)
+
+	os.Unsetenv("APPY_MASTER_KEY")
+	os.Unsetenv("HTTP_DEBUG_ENABLED")
+}
+
+func (s *ConfigSuite) TestNewConfigWithUndecryptableConfig() {
+	oldAppyEnv := os.Getenv("APPY_ENV")
+	os.Setenv("APPY_ENV", "undecryptable")
+	os.Setenv("APPY_MASTER_KEY", "481e5d98a31585148b8b1dfb6a3c0465")
+
+	build := DebugBuild
+	logger := NewLogger(build)
+	config := NewConfig(build, logger, nil)
+	s.Contains(config.Errors()[0].Error(), "unable to decrypt 'HTTP_PORT' value in 'testdata/pkg/config/.env.undecryptable'")
+
+	os.Setenv("APPY_ENV", oldAppyEnv)
+	os.Unsetenv("APPY_MASTER_KEY")
+}
+
+func (s *ConfigSuite) TestNewConfigWithUnparsableConfig() {
+	oldAppyEnv := os.Getenv("APPY_ENV")
+	os.Setenv("APPY_ENV", "unparsable")
+	os.Setenv("APPY_MASTER_KEY", "481e5d98a31585148b8b1dfb6a3c0465")
+
+	build := DebugBuild
+	logger := NewLogger(build)
+	config := NewConfig(build, logger, nil)
+	s.Contains(config.Errors()[0].Error(), "Can't separate key from value")
+
+	os.Setenv("APPY_ENV", oldAppyEnv)
+	os.Unsetenv("APPY_MASTER_KEY")
+}
+
+func (s *ConfigSuite) TestMasterKeyWithMissingKeyFile() {
+	_, err := parseMasterKey()
+	s.EqualError(err, ErrReadMasterKeyFile.Error())
+}
+
+func (s *ConfigSuite) TestMasterKeyWithMissingAppyMasterKey() {
+	Build = ReleaseBuild
+	_, err := parseMasterKey()
+	s.EqualError(err, ErrNoMasterKey.Error())
+	Build = DebugBuild
+}
+
+func (s *ConfigSuite) TestMasterKeyWithZeroLength() {
+	os.Setenv("APPY_MASTER_KEY", "")
+	Build = ReleaseBuild
+	_, err := parseMasterKey()
+	s.EqualError(err, ErrNoMasterKey.Error())
+	os.Unsetenv("APPY_MASTER_KEY")
+	Build = DebugBuild
 }
 
 func TestConfigSuite(t *testing.T) {
