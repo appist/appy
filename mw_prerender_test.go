87e47ded61abc8431a63ce9063432891670dd574
Cayter Goh <cayter1986@gmail.com>
2019-11-03 22:30:04 +0800

    Add more tests

--- mw_prerender_test.go
+++ mw_prerender_test.go
@@ -1,1 +1,109 @@
-package appy_test
+package appy
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"testing"
+)
+
+type PrerenderSuite struct {
+	TestSuite
+	config   *Config
+	logger   *Logger
+	recorder *httptest.ResponseRecorder
+}
+
+func (s *PrerenderSuite) SetupTest() {
+	os.Setenv("APPY_MASTER_KEY", "481e5d98a31585148b8b1dfb6a3c0465")
+	os.Setenv("HTTP_CSRF_SECRET", "481e5d98a31585148b8b1dfb6a3c0465")
+	os.Setenv("HTTP_SESSION_SECRETS", "481e5d98a31585148b8b1dfb6a3c0465")
+	s.logger = NewLogger(DebugBuild)
+	s.config = NewConfig(DebugBuild, s.logger, nil)
+	s.recorder = httptest.NewRecorder()
+}
+
+func (s *PrerenderSuite) TearDownTest() {
+	os.Unsetenv("APPY_MASTER_KEY")
+	os.Unsetenv("HTTP_CSRF_SECRET")
+	os.Unsetenv("HTTP_SESSION_SECRETS")
+}
+
+func (s *PrerenderSuite) TestRequestWithNonSEOBot() {
+	ctx, _ := CreateTestContext(s.recorder)
+	ctx.Request = &http.Request{
+		Header: map[string][]string{},
+		Method: "GET",
+		URL: &url.URL{
+			Path: "/",
+		},
+	}
+	Prerender(s.config, s.logger)(ctx)
+
+	s.Equal(200, ctx.Writer.Status())
+	s.Equal("", ctx.Writer.Header().Get(xPrerender))
+}
+
+func (s *PrerenderSuite) TestRequestHTTPHostWithSEOBot() {
+	ctx, _ := CreateTestContext(s.recorder)
+	ctx.Request = &http.Request{
+		Header: map[string][]string{},
+		Host:   "localhost",
+		Method: "GET",
+		URL: &url.URL{
+			Path: "/",
+		},
+	}
+	ctx.Request.Header.Add("User-Agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
+	Prerender(s.config, s.logger)(ctx)
+
+	s.Equal(200, ctx.Writer.Status())
+	s.Equal("1", ctx.Writer.Header().Get(xPrerender))
+}
+
+func (s *PrerenderSuite) TestRequestHTTPSHostWithSEOBot() {
+	ctx, _ := CreateTestContext(s.recorder)
+	ctx.Request = &http.Request{
+		Header: map[string][]string{},
+		Host:   "localhost",
+		Method: "GET",
+		URL: &url.URL{
+			Path: "/tools/about",
+		},
+	}
+	ctx.Request.Header.Add("User-Agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
+
+	s.config.HTTPSSLEnabled = true
+	Prerender(s.config, s.logger)(ctx)
+
+	s.Equal(200, ctx.Writer.Status())
+	s.Equal("1", ctx.Writer.Header().Get(xPrerender))
+}
+
+func (s *PrerenderSuite) TestRequestFailedWithSEOBot() {
+	oldCrawl := crawl
+	crawl = func(url string) ([]byte, error) {
+		return nil, errors.New("crawl failed")
+	}
+	ctx, _ := CreateTestContext(s.recorder)
+	ctx.Request = &http.Request{
+		Header: map[string][]string{},
+		Host:   "localhost",
+		Method: "GET",
+		URL: &url.URL{
+			Path: "/",
+		},
+	}
+	ctx.Request.Header.Add("User-Agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
+	Prerender(s.config, s.logger)(ctx)
+
+	s.Equal(500, ctx.Writer.Status())
+	s.Equal("", ctx.Writer.Header().Get(xPrerender))
+	crawl = oldCrawl
+}
+
+func TestPrerender(t *testing.T) {
+	RunTestSuite(t, new(PrerenderSuite))
+}
--- mw_util_test.go
+++ mw_util_test.go
@@ -1,1 +1,41 @@
 package appy_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/appist/appy"
+)
+
+type UtilSuite struct {
+	appy.TestSuite
+}
+
+func (s *UtilSuite) SetupTest() {
+}
+
+func (s *UtilSuite) TearDownTest() {
+}
+
+func (s *UtilSuite) TestIsAPIOnly() {
+	recorder := httptest.NewRecorder()
+
+	ctx, _ := appy.CreateTestContext(recorder)
+	ctx.Request = &http.Request{
+		Header: map[string][]string{},
+	}
+	ctx.Request.Header.Add("X-API-Only", "1")
+	s.Equal(true, appy.IsAPIOnly(ctx))
+
+	ctx, _ = appy.CreateTestContext(recorder)
+	ctx.Request = &http.Request{
+		Header: map[string][]string{},
+	}
+	ctx.Request.Header.Add("X-API-Only", "0")
+	s.Equal(false, appy.IsAPIOnly(ctx))
+}
+
+func TestUtil(t *testing.T) {
+	appy.RunTestSuite(t, new(UtilSuite))
+}
