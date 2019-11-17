62fc0c7a683bf0dcdfef69bb1d8483712ae06902
Cayter Goh <cayter1986@gmail.com>
2019-11-17 21:33:08 +0800

    Add more functions

--- internal/support/string.go
+++ internal/support/string.go
@@ -7,6 +7,39 @@ import (
 	"github.com/fatih/camelcase"
 )
 
+// IsCamelCase checks if a string is camelCase.
+func IsCamelCase(s string) bool {
+	if isFirstRuneDigit(s) {
+		return false
+	}
+
+	return isMadeByAlphanumeric(s) && isFirstRuneLower(s)
+}
+
+// IsChainCase checks if a string is a chain-case.
+func IsChainCase(s string) bool {
+	if strings.Contains(s, "-") {
+		fields := strings.Split(s, "-")
+		for _, field := range fields {
+			if !isMadeByLowerAndDigit(field) {
+				return false
+			}
+		}
+		return true
+	}
+
+	return isMadeByLowerAndDigit(s)
+}
+
+// IsFlatCase checks if a string is a flatcase.
+func IsFlatCase(s string) bool {
+	if isFirstRuneDigit(s) {
+		return false
+	}
+
+	return isMadeByLowerAndDigit(s)
+}
+
 // IsPascalCase checks if a string is a PascalCase.
 func IsPascalCase(str string) bool {
 	if isFirstRuneDigit(str) {
@@ -14,6 +47,22 @@ func IsPascalCase(str string) bool {
 	}
 
 	return isAlphanumeric(str) && isFirstRuneUpper(str)
+}
+
+// IsSnakeCase checks if a string is a snake_case.
+func IsSnakeCase(s string) bool {
+	if strings.Contains(s, "_") {
+		fields := strings.Split(s, "_")
+		for _, field := range fields {
+			if !isMadeByLowerAndDigit(field) {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	return isMadeByLowerAndDigit(s)
 }
 
 // ToCamelCase converts a string to camelCase style.
@@ -31,6 +80,26 @@ func ToCamelCase(str string) string {
 	return strings.Join(fields, "")
 }
 
+// ToChainCase converts a string to chain-case style.
+func ToChainCase(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	fields := splitToLowerFields(s)
+	return strings.Join(fields, "-")
+}
+
+// ToFlatCase converts a string to flatcase style.
+func ToFlatCase(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	fields := splitToLowerFields(s)
+	return strings.Join(fields, "")
+}
+
 // ToSnakeCase converts a string to snake_case style.
 func ToSnakeCase(str string) string {
 	if len(str) == 0 {
@@ -39,6 +108,15 @@ func ToSnakeCase(str string) string {
 
 	fields := splitToLowerFields(str)
 	return strings.Join(fields, "_")
+}
+
+func getRuneAt(s string, i int) rune {
+	if len(s) == 0 {
+		return 0
+	}
+
+	rs := []rune(s)
+	return rs[0]
 }
 
 func isAlphanumeric(s string) bool {
@@ -69,6 +147,42 @@ func isFirstRuneUpper(s string) bool {
 	}
 
 	return unicode.IsUpper(runeAt(s, 0))
+}
+
+func isFirstRuneLower(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	return unicode.IsLower(getRuneAt(s, 0))
+}
+
+func isMadeByAlphanumeric(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	for _, r := range s {
+		if !unicode.IsUpper(r) && !unicode.IsLower(r) && !unicode.IsDigit(r) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isMadeByLowerAndDigit(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	for _, r := range s {
+		if !unicode.IsLower(r) && !unicode.IsDigit(r) {
+			return false
+		}
+	}
+
+	return true
 }
 
 func runeAt(s string, i int) rune {
--- internal/support/string_test.go
+++ internal/support/string_test.go
@@ -16,6 +16,73 @@ func (s *StringSuite) SetupTest() {
 func (s *StringSuite) TearDownTest() {
 }
 
+func (s *StringSuite) TestIsCamelCase() {
+	tt := [][]interface{}{
+		{"fooBar", true},
+		{"fooBar1", true},
+		{"foo1Bar", true},
+		{"", false},
+		{"1FooBar", false},
+		{"1fooBar", false},
+		{"foo@Bar", false},
+		{"foo_bar", false},
+		{"FOO_BAR", false},
+		{"FooBar", false},
+		{"Foo@Bar", false},
+	}
+
+	for _, t := range tt {
+		s.Equal(t[1], IsCamelCase(t[0].(string)))
+	}
+}
+
+func (s *StringSuite) TestIsChainCase() {
+	tt := [][]interface{}{
+		{"foo-bar", true},
+		{"foo1-bar2", true},
+		{"foo-bar-1", true},
+		{"foo-bar-1", true},
+		{"111-foo-bar", true},
+		{"foobar", true},
+		{"foobar1", true},
+		{"foo1bar", true},
+		{"1foobar", true},
+		{"", false},
+		{"FOO-BAR", false},
+		{"fooBar", false},
+		{"FooBar", false},
+		{"FOOBAR", false},
+		{"foo-@bar", false},
+		{"foo_bar", false},
+		{"テスト", false},
+		{"テスト-テスト", false},
+	}
+
+	for _, t := range tt {
+		s.Equal(t[1], IsChainCase(t[0].(string)))
+	}
+}
+
+func (s *StringSuite) TestIsFlatCase() {
+	tt := [][]interface{}{
+		{"foobar", true},
+		{"foo1bar", true},
+
+		{"", false},
+		{"1foobar", false},
+		{"foo@bar", false},
+		{"foo_bar", false},
+		{"FOO_BAR", false},
+		{"FooBar", false},
+		{"fooBar", false},
+		{"foo_bar", false},
+	}
+
+	for _, t := range tt {
+		s.Equal(t[1], IsFlatCase(t[0].(string)))
+	}
+}
+
 func (s *StringSuite) TestIsPascalCase() {
 	tt := [][]interface{}{
 		{"FooBar", true},
@@ -33,6 +100,33 @@ func (s *StringSuite) TestIsPascalCase() {
 
 	for _, t := range tt {
 		s.Equal(t[1], IsPascalCase(t[0].(string)))
+	}
+}
+
+func (s *StringSuite) TestIsSnakeCase() {
+	tt := [][]interface{}{
+		{"foo_bar", true},
+		{"foo1_bar2", true},
+		{"foo_bar_1", true},
+		{"foo_bar_1", true},
+		{"111_foo_bar", true},
+		{"foobar", true},
+		{"foobar1", true},
+		{"foo1bar", true},
+		{"111foobar", true},
+		{"", false},
+		{"FOO_BAR", false},
+		{"fooBar", false},
+		{"FooBar", false},
+		{"FOOBAR", false},
+		{"foo_@bar", false},
+		{"foo-bar", false},
+		{"テスト", false},
+		{"テスト_テスト", false},
+	}
+
+	for _, t := range tt {
+		s.Equal(t[1], IsSnakeCase(t[0].(string)))
 	}
 }
 
@@ -54,6 +148,48 @@ func (s *StringSuite) TestToCamelCase() {
 
 	for _, t := range tt {
 		s.Equal(t[1], ToCamelCase(t[0]))
+	}
+}
+
+func (s *StringSuite) TestToChainCase() {
+	tt := [][]string{
+		{"foo_bar", "foo-bar"},
+		{"foo-bar", "foo-bar"},
+		{"foo-bar_baz", "foo-bar-baz"},
+		{"foo--bar__baz", "foo-bar-baz"},
+		{"fooBar", "foo-bar"},
+		{"FooBar", "foo-bar"},
+		{"foo bar", "foo-bar"},
+		{"   foo   bar   ", "foo-bar"},
+		{"fooBar111", "foo-bar-111"},
+		{"111FooBar", "111-foo-bar"},
+		{"foo-111-Bar", "foo-111-bar"},
+		{"", ""},
+	}
+
+	for _, t := range tt {
+		s.Equal(t[1], ToChainCase(t[0]))
+	}
+}
+
+func (s *StringSuite) TestToFlatCase() {
+	tt := [][]string{
+		{"foo_bar", "foobar"},
+		{"foo-bar", "foobar"},
+		{"foo-bar_baz", "foobarbaz"},
+		{"foo--bar__baz", "foobarbaz"},
+		{"fooBar", "foobar"},
+		{"FooBar", "foobar"},
+		{"foo bar", "foobar"},
+		{"   foo   bar   ", "foobar"},
+		{"fooBar111", "foobar111"},
+		{"111FooBar", "111foobar"},
+		{"foo-111-Bar", "foo111bar"},
+		{"", ""},
+	}
+
+	for _, t := range tt {
+		s.Equal(t[1], ToFlatCase(t[0]))
 	}
 }
 
