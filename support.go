0ef2008bac8320ae0bfcc2567bcddcf8263b8579
cayter <cayter1986@gmail.com>
2020-01-09 00:17:17 +0800

    feat(support): add `ArrayContains`

--- support.go
+++ support.go
@@ -16,6 +16,7 @@ import (
 type (
 	// Supporter satisfies Support type and implements all its functions, mainly used for unit testing's mock.
 	Supporter interface {
+		ArrayContains(arr interface{}, val interface{}) bool
 		AESDecrypt(ciphertext []byte, key []byte) ([]byte, error)
 		AESEncrypt(plaintext []byte, key []byte) ([]byte, error)
 		CaptureOutput(f func()) string
@@ -25,6 +26,123 @@ type (
 	// Support contains the useful functions.
 	Support struct{}
 )
+
+// ArrayContains checks if a value is in a slice of the same type.
+func (s *Support) ArrayContains(arr interface{}, val interface{}) bool {
+	arrT := reflect.TypeOf(arr)
+	valT := reflect.TypeOf(val)
+	if (arrT.Kind().String() != "array" && arrT.Kind().String() != "slice") ||
+		arrT.Elem().String() != valT.Kind().String() {
+		return false
+	}
+
+	switch arr := arr.(type) {
+	case []bool:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []byte:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []complex64:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []complex128:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []float32:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []float64:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []int:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []int8:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []int16:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []int32:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []int64:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []uint:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []uint16:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []uint32:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []uint64:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []uintptr:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	case []string:
+		for _, a := range arr {
+			if a == val {
+				return true
+			}
+		}
+	}
+
+	return false
+}
 
 // AESDecrypt decrypts a cipher text into a plain text using the key with AES.
 func (s *Support) AESDecrypt(ciphertext []byte, key []byte) ([]byte, error) {
