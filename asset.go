2ba4ce53d7f9e9c9efbf68481565870584173aa0
cayter <cayter1986@gmail.com>
2020-01-08 22:42:37 +0800

    feat(asset): add `asset` type

---
+++ asset.go
@@ -0,0 +1,89 @@
+package appy
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+// Asset manages the application assets.
+type Asset struct {
+	embedded http.FileSystem
+	layout   map[string]string
+}
+
+// NewAsset initializes the assets instance.
+func NewAsset(embedded http.FileSystem, layout map[string]string) *Asset {
+	asset := &Asset{
+		embedded: embedded,
+		layout: map[string]string{
+			"config": "configs",
+			"docker": ".docker",
+			"locale": "pkg/locales",
+			"view":   "pkg/views",
+			"web":    "web",
+		},
+	}
+
+	if layout != nil {
+		asset.layout = layout
+	}
+
+	return asset
+}
+
+// Layout returns the appy's project layout.
+func (a *Asset) Layout() map[string]string {
+	return a.layout
+}
+
+// Open opens the named file for reading. If the current build type is debug, reads from the filesystem. Otherwise, it
+// reads from the embedded static assets which is a virtual file system.
+func (a *Asset) Open(path string) (io.Reader, error) {
+	if IsDebugBuild() {
+		return os.Open(path)
+	}
+
+	if a.embedded == nil {
+		return nil, ErrNoEmbeddedAssets
+	}
+
+	return a.embedded.Open(path)
+}
+
+// ReadDir returns a list of file/directory entries in the directory.
+func (a *Asset) ReadDir(dir string) ([]os.FileInfo, error) {
+	if IsDebugBuild() {
+		return ioutil.ReadDir(dir)
+	}
+
+	if a.embedded == nil {
+		return nil, ErrNoEmbeddedAssets
+	}
+
+	reader, err := a.embedded.Open(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	return reader.Readdir(-1)
+}
+
+// ReadFile returns the content of the filename.
+func (a *Asset) ReadFile(filename string) ([]byte, error) {
+	if IsDebugBuild() {
+		return ioutil.ReadFile(filename)
+	}
+
+	if a.embedded == nil {
+		return nil, ErrNoEmbeddedAssets
+	}
+
+	file, err := a.embedded.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(file)
+}
---
+++ asset_test.go
@@ -0,0 +1,135 @@
+package appy_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/appist/appy"
+)
+
+type AssetSuite struct {
+	appy.TestSuite
+}
+
+func (s *AssetSuite) TestNewAsset() {
+	asset := appy.NewAsset(nil, nil)
+
+	s.Equal("configs", asset.Layout()["config"])
+	s.Equal(".docker", asset.Layout()["docker"])
+	s.Equal("pkg/locales", asset.Layout()["locale"])
+	s.Equal("pkg/views", asset.Layout()["view"])
+	s.Equal("web", asset.Layout()["web"])
+
+	asset = appy.NewAsset(nil, map[string]string{
+		"config": "testdata/configs",
+		"docker": "testdata/.docker",
+		"locale": "testdata/pkg/locales",
+		"view":   "testdata/pkg/views",
+		"web":    "testdata/web",
+	})
+
+	s.Equal("testdata/configs", asset.Layout()["config"])
+	s.Equal("testdata/.docker", asset.Layout()["docker"])
+	s.Equal("testdata/pkg/locales", asset.Layout()["locale"])
+	s.Equal("testdata/pkg/views", asset.Layout()["view"])
+	s.Equal("testdata/web", asset.Layout()["web"])
+}
+
+func (s *AssetSuite) TestOpen() {
+	asset := appy.NewAsset(nil, map[string]string{
+		"config": "testdata/asset/open/configs",
+		"docker": "testdata/asset/open/.docker",
+		"locale": "testdata/asset/open/pkg/locales",
+		"view":   "testdata/asset/open/pkg/views",
+		"web":    "testdata/asset/open/web",
+	})
+	_, err := asset.Open("testdata/asset/open/configs/.env.development")
+	s.EqualError(err, "open testdata/asset/open/configs/.env.development: no such file or directory")
+
+	_, err = asset.Open("testdata/asset/open/pkg/locales/en.yml")
+	s.NoError(err)
+
+	appy.Build = appy.ReleaseBuild
+	defer func() {
+		appy.Build = appy.DebugBuild
+	}()
+
+	asset = appy.NewAsset(nil, nil)
+	_, err = asset.Open("configs/.env.development")
+	s.EqualError(err, appy.ErrNoEmbeddedAssets.Error())
+
+	asset = appy.NewAsset(http.Dir("testdata/asset/open"), nil)
+	_, err = asset.Open("configs/.env.development")
+	s.EqualError(err, "open testdata/asset/open/configs/.env.development: no such file or directory")
+
+	_, err = asset.Open("pkg/locales/en.yml")
+	s.NoError(err)
+}
+
+func (s *AssetSuite) TestReadDir() {
+	asset := appy.NewAsset(nil, map[string]string{
+		"config": "testdata/asset/open/configs",
+		"docker": "testdata/asset/open/.docker",
+		"locale": "testdata/asset/open/pkg/locales",
+		"view":   "testdata/asset/open/pkg/views",
+		"web":    "testdata/asset/open/web",
+	})
+	_, err := asset.ReadDir("testdata/asset/missing")
+	s.EqualError(err, "open testdata/asset/missing: no such file or directory")
+
+	_, err = asset.ReadDir("testdata/asset/readdir")
+	s.NoError(err)
+
+	appy.Build = appy.ReleaseBuild
+	defer func() {
+		appy.Build = appy.DebugBuild
+	}()
+
+	asset = appy.NewAsset(nil, nil)
+	_, err = asset.ReadDir("configs")
+	s.EqualError(err, appy.ErrNoEmbeddedAssets.Error())
+
+	asset = appy.NewAsset(http.Dir("testdata/asset/readdir"), nil)
+	_, err = asset.ReadDir("configs")
+	s.EqualError(err, "open testdata/asset/readdir/configs: no such file or directory")
+
+	fis, err := asset.ReadDir("pkg")
+	s.NoError(err)
+	s.Equal(2, len(fis))
+}
+
+func (s *AssetSuite) TestReadFile() {
+	asset := appy.NewAsset(nil, map[string]string{
+		"config": "testdata/asset/open/configs",
+		"docker": "testdata/asset/open/.docker",
+		"locale": "testdata/asset/open/pkg/locales",
+		"view":   "testdata/asset/open/pkg/views",
+		"web":    "testdata/asset/open/web",
+	})
+	_, err := asset.ReadFile("testdata/asset/missing/pkg/locales/en.yml")
+	s.EqualError(err, "open testdata/asset/missing/pkg/locales/en.yml: no such file or directory")
+
+	_, err = asset.ReadFile("testdata/asset/readfile/pkg/locales/en.yml")
+	s.NoError(err)
+
+	appy.Build = appy.ReleaseBuild
+	defer func() {
+		appy.Build = appy.DebugBuild
+	}()
+
+	asset = appy.NewAsset(nil, nil)
+	_, err = asset.ReadFile("pkg/locales/en.yml")
+	s.EqualError(err, appy.ErrNoEmbeddedAssets.Error())
+
+	asset = appy.NewAsset(http.Dir("testdata/asset/readfile"), nil)
+	_, err = asset.ReadFile("pkg/locales/en-US.yml")
+	s.EqualError(err, "open testdata/asset/readfile/pkg/locales/en-US.yml: no such file or directory")
+
+	data, err := asset.ReadFile("pkg/locales/en.yml")
+	s.NoError(err)
+	s.Equal("test: Test\n", string(data))
+}
+
+func TestAssetSuite(t *testing.T) {
+	appy.RunTestSuite(t, new(AssetSuite))
+}
