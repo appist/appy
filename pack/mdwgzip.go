f7a86d0e2b725dc3c282ec44128b41aeae34d02f
cayter <cayter1986@gmail.com>
2020-04-09 02:11:31 +0800

    remove redundant method

--- pack/mdwgzip.go
+++ pack/mdwgzip.go
@@ -107,10 +107,6 @@ func (g *gzipWriter) WriteString(s string) (int, error) {
 	return g.writer.Write([]byte(s))
 }
 
-func (g *gzipWriter) Write(data []byte) (int, error) {
-	return g.writer.Write(data)
-}
-
 func (g *gzipWriter) WriteHeader(code int) {
 	g.Header().Del("Content-Length")
 	g.ResponseWriter.WriteHeader(code)
